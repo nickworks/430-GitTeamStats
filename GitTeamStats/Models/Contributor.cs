c7f90ccc801f180a4c8892b9f9e324622476589c
Nick <pattison.nick@gmail.com>
2019-10-02 23:09:09 -0400

    Added a few Models and some additional packages
    + Models.RepoLoader class : opens repos
    + Models.Contributor class : a nifty class "repo contributors"
    + Windows7APICodePacks (Core and Shell) just so we could have a "open folder" dialog.

---
+++ GitTeamStats/Models/Contributor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace GitTeamStats.Models {
+    /// <summary>
+    /// This class represents a single contributor to a repo.
+    /// Each contributor has a unique email address.
+    /// </summary>
+    public class Contributor {
+        /// <summary>
+        /// The username of the contributor.
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// The email address of the contributor.
+        /// </summary>
+        public string email;
+        /// <summary>
+        /// A list of all commits authored by the contributor.
+        /// </summary>
+        public List<Commit> commits = new List<Commit>();
+        /// <summary>
+        /// This constructor creates a contributor from a supplied commit.
+        /// It grabs contributor info from the commit's Committer member.
+        /// </summary>
+        /// <param name="commit">A commit authored by the new Contributor</param>
+        public Contributor(Commit commit) {
+            name = commit.Committer.Name;
+            email = commit.Committer.Email;
+            commits.Add(commit);
+        }
+        /// <summary>
+        /// This method will scan an entire repo and return a list of all contributors.
+        /// </summary>
+        /// <param name="repo">A specified repo</param>
+        /// <returns>A list of all contributors</returns>
+        static public List<Contributor> GetAll(Repository repo) {
+            List<Contributor> contributors = new List<Contributor>();
+
+            foreach (Commit commit in repo.Commits) {
+
+                string user = commit.Committer.Email;
+
+                Contributor contributor = contributors.Find(c => c.email == user);
+                if (contributor != null) { // if contributor exists:
+                    contributor.commits.Add(commit); // add commit to contributor's list of commits
+                } else { // if contributor does NOT exist:
+                    contributors.Add(new Contributor(commit)); // create contributor
+                }
+            }
+            return contributors;
+        }
+    }
+}
---
+++ GitTeamStats/Models/RepoLoader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibGit2Sharp;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace GitTeamStats.Models {
+    /// <summary>
+    /// This class provides a few wrapper functions for opening repos.
+    /// </summary>
+    static public class RepoLoader {
+        /// <summary>
+        /// This method shows a dialog box that allows the user
+        /// to navigate to a local repo.
+        /// </summary>
+        /// <returns>A repository. If failed or canceled, returns null.</returns>
+        static public Repository ShowDialog() {
+            CommonOpenFileDialog d = new CommonOpenFileDialog();
+            d.IsFolderPicker = true;
+
+            CommonFileDialogResult result = d.ShowDialog();
+            switch (result) {
+                case CommonFileDialogResult.Ok:
+                    return OpenFromPath(d.FileName);
+                case CommonFileDialogResult.None:
+                    break;
+                case CommonFileDialogResult.Cancel:
+                    break;
+            }
+            
+            return null;
+        }
+        /// <summary>
+        /// This method attempts to open a repo from a supplied path.
+        /// </summary>
+        /// <param name="pathToRepo">The path to a repo folder</param>
+        /// <returns>A repository. If failed, returns null.</returns>
+        static public Repository OpenFromPath(string pathToRepo) {
+            try {
+                Repository repo = new Repository(pathToRepo);
+                return repo;
+            } catch (RepositoryNotFoundException e) {
+                // TODO: handle error
+            }
+            return null;
+        }
+
+    }
+}
